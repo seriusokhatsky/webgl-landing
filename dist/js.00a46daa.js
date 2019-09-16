f281a2a60fac357002af36ca7e21de7b4035db34
sergsokhatskiy <seriu.1995@gmail.com>
2019-09-16 17:56:02 +0300

    buid

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -53330,7 +53330,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "19431" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "29818" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
